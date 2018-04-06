9f81061d31bafc4e9b74b5f73e18d214f30552e5
purwnt <purwanto1337@gmail.com>
2018-04-06 02:09:31 +0000

    perbaikan

--- public/javascripts/countads.js
+++ public/javascripts/countads.js
@@ -1,5 +1,5 @@
 function myFunction(idads) {
-    $.getJSON("http://103.253.115.140/ads/viewcount/"+idads, function(json){
+    $.getJSON("http://103.253.115.140:3000/ads/viewcount/"+idads, function(json){
         document.getElementById("count").innerHTML = json.total
       });
     document.getElementById("ads").innerHTML = idads
